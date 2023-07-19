dbb3ecb34bb7d9b2258c52a0c8b37a37b72959f2
이창호 <ckdgh518@naver.com>
2023-07-19 20:09:11 +0900

    add account app

---
+++ account/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from account import views
+from base_project.routers import CustomRouter
+
+custom_router = DefaultRouter()
+custom_router.include_root_view = False
+custom_router.register('', views.UserViewSet, basename="user")
+
+urlpatterns = [
+    path('', include(custom_router.urls)),
+]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from account.models import User
+from account.serializers import UserSerializer
+from base_project.pagination import get_pagination_class, PageNumberPagination
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-id")
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    pagination_class = get_pagination_class(5)
