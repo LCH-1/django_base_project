a690a4a6fe57c0b89a2ada9a8314a58dd3380326
이창호 <ckdgh518@naver.com>
2023-07-19 20:10:13 +0900

    change account app name to user

---
+++ user/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+
+from user.forms import UserChangeForm, UserCreationForm
+from user.models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('id', 'email', 'join_date', 'is_active', 'is_admin')
+
+    list_display_links = ('email', )
+    list_filter = ('is_admin', 'is_active', )
+
+    fieldsets = (
+        ('Personal info', {'fields': ('email', 'password', 'fullname', )}),
+        ('Status', {'fields': ('is_active', )}),
+        ('Permissions', {'fields': ('is_admin', )}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_admin', )}
+         ),
+    )
+
+    search_fields = ('email', 'fullname', )
+    ordering = ('-id',)
+
+    filter_horizontal = []
+
+    inlines = []
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('email', 'join_date', )
+        else:
+            return ('join_date', )
+
+
+admin.site.register(User, UserAdmin)
+
+# unregister default group
+admin.site.unregister(Group)
---
+++ user/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from user.models import User
+from user.serializers import UserSerializer
+from base_project.pagination import get_pagination_class, PageNumberPagination
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-id")
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    pagination_class = get_pagination_class(5)
