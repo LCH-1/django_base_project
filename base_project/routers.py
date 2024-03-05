ad9f06ac1ae3aaaa1d43c208dc84f207998cf7a9
이창호 <ckdgh518@naver.com>
2024-03-05 19:19:07 +0900

    router 이름 변경, custom action url router 추가

--- base_project/routers.py
+++ base_project/routers.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import Route, DynamicRoute, DefaultRouter
 
 
-class CustomRouter(DefaultRouter):
+class UserRouter(DefaultRouter):
     routes = [
         Route(
             url=r'^{prefix}{trailing_slash}$',
@@ -23,6 +23,57 @@ class CustomRouter(DefaultRouter):
         ),
         DynamicRoute(
             url=r'^{prefix}/{lookup}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=True,
+            initkwargs={}
+        ),
+    ]
+
+
+class CustomActionUrlRouter(DefaultRouter):
+    """
+        action(detail=True)로 설정 시 url 구조 변경
+        기본 url 구조
+         - ${pk}/${action_url}/
+        수정된 url 구조
+         - ${action_url}/${pk(optional)}/
+    """
+    routes = [
+        # List route.
+        Route(
+            url=r'^{prefix}{trailing_slash}$',
+            mapping={
+                'get': 'list',
+                'post': 'create'
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        # Dynamically generated list routes. Generated using
+        # @action(detail=False) decorator on methods of the viewset.
+        DynamicRoute(
+            url=r'^{prefix}/{url_path}{trailing_slash}$',
+            name='{basename}-{url_name}',
+            detail=False,
+            initkwargs={}
+        ),
+        # Detail route.
+        Route(
+            url=r'^{prefix}/{lookup}{trailing_slash}$',
+            mapping={
+                'get': 'retrieve',
+                'put': 'update',
+                'patch': 'partial_update',
+                'delete': 'destroy'
+            },
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Instance'}
+        ),
+
+        DynamicRoute(
+            url=r'^{prefix}/{url_path}/?(?P<pk>[^/.]+)?{trailing_slash}$',
             name='{basename}-{url_name}',
             detail=True,
             initkwargs={}
--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from user import views
-from base_project.routers import CustomRouter
+from base_project.routers import UserRouter
 
-custom_router = DefaultRouter()
+custom_router = UserRouter()
 custom_router.include_root_view = False
 custom_router.register('', views.UserViewSet, basename="user")
 
